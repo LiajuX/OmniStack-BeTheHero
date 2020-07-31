af615f6911c5a50d8f9e10195b44c5e0dea57252
LiajuX <julialdbrito@gmail.com>
2020-07-31 18:26:15 -0300

    First Commit

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Mandar a aplicação ouvir a porta 3333
+app.listen(3333);
